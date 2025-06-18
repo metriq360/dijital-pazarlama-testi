ae11c861a80e3c6adc4ec9f8cea2e8e6ad254474
metriq360 <metriq360@gmail.com>
2025-06-18 03:19:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  build: {
+    // Bu kısım, Rollup'ın CommonJS modüllerini (bazı kütüphaneler gibi react-markdown'ın bazı parçaları da böyle olabilir)
+    // doğru bir şekilde işlemesini ve uygulamanıza dahil etmesini sağlar.
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
+});
