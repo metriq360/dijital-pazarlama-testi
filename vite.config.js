f1fbc16b938411f6e1a7a6886b76d44d3e9a5fcb
metriq360 <metriq360@gmail.com>
2025-06-18 03:49:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: { // Bu yeni kısım
+    include: ['react-markdown'], // `react-markdown`'ı açıkça optimize etmesini söylüyoruz
+  },
   build: {
     commonjsOptions: {
       include: [/node_modules/],
